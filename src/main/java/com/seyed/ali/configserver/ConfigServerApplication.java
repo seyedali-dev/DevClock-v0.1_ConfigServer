eed72ebd446ebee55e75f00e63bb6d3d6ded7cee
SayedAli <sayedxali7@gmail.com>
2024-06-04 16:24:51 +0330

    config-server initial commit

---
+++ src/main/java/com/seyed/ali/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.seyed.ali.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
